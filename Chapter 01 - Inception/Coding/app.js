734f8de20be7a037460707fb296d788875206dee
Yogini <yoginikokare@gmail.com>
2023-01-10 16:12:12 +0530

    Chapter-01-Inception

---
+++ Chapter 01 - Inception/Coding/app.js
@@ -0,0 +1,40 @@
+//using JavaScript
+//const heading = document.createElement("h1"); //element creation
+//heading.innerText("Namste Javascript"); //assigning value
+//const root = document.getElementById("root"); //creating root inside which wnt to place element
+//root.appendChild("heading"); //rendering into root
+
+//using react
+//(converted to)
+//reactElement ==============> object =======> HTML(DOM) element
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Heading 1"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "Heading 2"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading2]
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+//It will override all the existing content which is inside root
+root.render(container);
